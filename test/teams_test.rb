f4f65ae6d2aa13bdbd60c5fc9beaac3808507ae1
ignored-comment <info.konham@gmail.com>
2021-03-03 14:15:27 -0500

    Finalize tests for game and team test

--- test/teams_test.rb
+++ test/teams_test.rb
@@ -1,14 +1,14 @@
-require 'simplecov'
+require 'CSV'
 require 'minitest/autorun'
 require 'minitest/pride'
-require './lib/stat_tracker'
-# require './test/test_helper'
 require './lib/teams'
+require './lib/stat_tracker'
 require './lib/teams_manager'
 require './lib/games_manager'
 require './lib/game_teams_manager'
 
-class TeamsTest < Minitest::Test
+
+class TeamTest < Minitest::Test
   def setup
     @game_path = './data/games.csv'
     @team_path = './data/teams.csv'
@@ -23,6 +23,24 @@ class TeamsTest < Minitest::Test
 
     @stat_tracker = StatTracker.from_csv(@locations)
     @data = @stat_tracker.load_csv(@locations[:teams])
-    @team = Team.new(@data, @stat_tracker)
+    @team_manager = TeamManager.new(@data, @stat_tracker)
+    @team = @team_manager.teams[0]
+  end
+
+  def test_it_exists
+    assert_instance_of Team, @team
+  end
+
+  def test_it_has_readable_attributes
+    assert_equal "1", @team.team_id
+    assert_equal "23", @team.franchise_id
+    assert_equal "Atlanta United", @team.team_name
+    assert_equal "ATL", @team.abbreviation
+    assert_equal "/api/v1/teams/1", @team.link
+    assert_instance_of TeamManager, @team.manager
+  end
+
+  def test_team_info_returns_hash
+    assert_instance_of Hash, @team.team_info
   end
 end
